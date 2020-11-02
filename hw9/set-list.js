c75bbd948e316f64861bd9061c2fa54516212dc6
Elizabeth Hom <hom.elizabeth@yahoo.com>
2020-11-02 18:57:55 -0500

    Converted to spaces

--- hw9/set-list.js
+++ hw9/set-list.js
@@ -1,52 +1,52 @@
-	var setList = [
-		{
-			"Artist": "Japanese Breakfast",
-			"Title": "Road Head",
-			"Genre": ["Indie"],
-			"Year": 2017
-		}, {
-			"Artist": "AJ Mitchell",
-			"Title": "Down In Flames",
-			"Genre": ["Pop"],
-			"Year": 2019
-		}, {
-			"Artist": "One Direction",
-			"Title": "What Makes You Beautiful",
-			"Genre": ["Pop"],
-			"Year": 2011
-		}, {
-			"Artist": "Secondhand Serenade",
-			"Title": "Fall For You",
-			"Genre": ["Indie", "Pop"],
-			"Year": 2008
-		}, {
-			"Artist": "Eric Nam",
-			"Title": "Congratulations",
-			"Genre": ["K-Pop", "Pop"],
-			"Year": 2019
-		}, {
-			"Artist": "Lewis Capaldi",
-			"Title": "Before You Go",
-			"Genre": ["Indie"],
-			"Year": 2019
-		}, {
-			"Artist": "Alec Benjamin",
-			"Title": "Let Me Down Slowly",
-			"Genre": ["Pop", "Indie"],
-			"Year": 2018
-		}, {
-			"Artist": "FINNEAS",
-			"Title": "Let's Fall in Love for the Night",
-			"Genre": ["Indie"],
-			"Year": 2019
-		}, {
-			"Artist": "Coldplay",
-			"Title": "Viva la Vida",
-			"Genre": ["Pop"],
-			"Year": 2015
-		}, {
-			"Artist": "Panic! At The Disco",
-			"Title": "House of Memories",
-			"Genre": ["Pop", "Rock"],
-			"Year": 2016
-		}];
+var setList = [
+    {
+        "Artist": "Japanese Breakfast",
+        "Title": "Road Head",
+        "Genre": ["Indie"],
+        "Year": 2017
+    }, {
+        "Artist": "AJ Mitchell",
+        "Title": "Down In Flames",
+        "Genre": ["Pop"],
+        "Year": 2019
+    }, {
+        "Artist": "One Direction",
+        "Title": "What Makes You Beautiful",
+        "Genre": ["Pop"],
+        "Year": 2011
+    }, {
+        "Artist": "Secondhand Serenade",
+        "Title": "Fall For You",
+        "Genre": ["Indie", "Pop"],
+        "Year": 2008
+    }, {
+        "Artist": "Eric Nam",
+        "Title": "Congratulations",
+        "Genre": ["K-Pop", "Pop"],
+        "Year": 2019
+    }, {
+        "Artist": "Lewis Capaldi",
+        "Title": "Before You Go",
+        "Genre": ["Indie"],
+        "Year": 2019
+    }, {
+        "Artist": "Alec Benjamin",
+        "Title": "Let Me Down Slowly",
+        "Genre": ["Pop", "Indie"],
+        "Year": 2018
+    }, {
+        "Artist": "FINNEAS",
+        "Title": "Let's Fall in Love for the Night",
+        "Genre": ["Indie"],
+        "Year": 2019
+    }, {
+        "Artist": "Coldplay",
+        "Title": "Viva la Vida",
+        "Genre": ["Pop"],
+        "Year": 2015
+    }, {
+        "Artist": "Panic! At The Disco",
+        "Title": "House of Memories",
+        "Genre": ["Pop", "Rock"],
+        "Year": 2016
+    }];
